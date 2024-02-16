52a600208f51ac23f2e88b120e9631f476e3514d
Rodney Blevins <rodney@blevins.nl>
2024-02-16 15:09:20 -0700

    Remove unused modules

--- src/main.js
+++ src/main.js
@@ -1,11 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import vue3GoogleLogin from 'vue3-google-login'
 
 const app = createApp(App)
-
-app.use(vue3GoogleLogin, {
-  clientId: '729581094163-ibuug8oaa9gb0hb2fde3b6mei5343v5e.apps.googleusercontent.com'
-})
 
 app.mount('#app')
